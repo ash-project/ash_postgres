a4f24d0cdc997e23dba471db7666c9ab6b3e007a
Zach Daniel <zachary.s.daniel@gmail.com>
2019-12-08 14:06:21 -0500

    WIP

--- mix.exs
+++ mix.exs
@@ -35,7 +35,8 @@ defmodule AshPostgres.MixProject do
     [
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
-      {:ash, "~> 0.1.0"},
+      {:ash, "~> 0.1.1"},
+      {:ashton, "~> 0.4.0"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
