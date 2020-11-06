aaeca65df433a7c2e575d5ad6731e0c1bb7537c2
Zach Daniel <zachary.s.daniel@gmail.com>
2020-11-05 21:19:51 -0500

    improvement: add utc_datetime migration type

--- lib/migration_generator/migration_generator.ex
+++ lib/migration_generator/migration_generator.ex
@@ -982,6 +982,7 @@ defmodule AshPostgres.MigrationGenerator do
   defp migration_type(:integer), do: :integer
   defp migration_type(:boolean), do: :boolean
   defp migration_type(:binary_id), do: :binary_id
+  defp migration_type(:utc_datetime), do: :utc_datetime
   defp migration_type(other), do: raise("No migration_type set up for #{other}")
 
   defp foreign_key?(relationship) do
