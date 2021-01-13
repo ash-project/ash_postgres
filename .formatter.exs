e63cf883e54ac2caecb4053ec49b128c760744de
Zach Daniel <zachary.s.daniel@gmail.com>
2021-01-12 20:48:17 -0500

    chore: fix formatter

--- .formatter.exs
+++ .formatter.exs
@@ -12,7 +12,6 @@ locals_without_parens = [
 ]
 
 [
-  import_deps: [:ash],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
   locals_without_parens: locals_without_parens,
   export: [
