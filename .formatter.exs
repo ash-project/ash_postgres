a9db1f3601030aa4fb9e81f8a15eb12bf9ebf7b6
zimt28 <1764689+zimt28@users.noreply.github.com>
2021-01-13 02:16:48 +0100

    chore: Update primary keys & formatting (#44)

--- .formatter.exs
+++ .formatter.exs
@@ -12,6 +12,7 @@ locals_without_parens = [
 ]
 
 [
+  import_deps: [:ash],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
   locals_without_parens: locals_without_parens,
   export: [
