058d8eb45c887dc8bca40b0fb3076dbaad1a0482
Zach Daniel <zach@zachdaniel.dev>
2021-05-09 17:44:23 -0400

    improvement: update to the latest ash version

--- mix.exs
+++ mix.exs
@@ -95,7 +95,7 @@ defmodule AshPostgres.MixProject do
       {:ecto_sql, "~> 3.5"},
       {:jason, "~> 1.0"},
       {:postgrex, ">= 0.0.0"},
-      {:ash, ash_version("~> 1.43 and >= 1.43.9")},
+      {:ash, ash_version("~> 1.43 and >= 1.43.11")},
       {:git_ops, "~> 2.4.2", only: :dev},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
       {:ex_check, "~> 0.11.0", only: :dev},
