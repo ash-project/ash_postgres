18af582c7c055fb31b95f4c3ff1efe4051890ebe
Zach Daniel <zach@zachdaniel.dev>
2021-07-05 14:24:41 -0400

    chore: fix lint

--- .credo.exs
+++ .credo.exs
@@ -116,7 +116,7 @@
         ## Refactoring Opportunities
         #
         {Credo.Check.Refactor.CondStatements, []},
-        {Credo.Check.Refactor.CyclomaticComplexity, [max_complexity: 16]},
+        {Credo.Check.Refactor.CyclomaticComplexity, [max_complexity: 18]},
         {Credo.Check.Refactor.FunctionArity, []},
         {Credo.Check.Refactor.LongQuoteBlocks, []},
         {Credo.Check.Refactor.MapInto, []},
