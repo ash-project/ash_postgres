df62c35801d6a9bb4cc117b985c15f99ff16ca68
kernel-io <kernel-io@users.noreply.github.com>
2021-11-25 13:31:59 +1300

    improvement: don't upsert defaults on conflict (#77)
    Co-authored-by: kernel-io <kernel>

--- mix.exs
+++ mix.exs
@@ -96,7 +96,7 @@ defmodule AshPostgres.MixProject do
       {:ecto_sql, "~> 3.7"},
       {:jason, "~> 1.0"},
       {:postgrex, ">= 0.0.0"},
-      {:ash, ash_version("~> 1.50")},
+      {:ash, ash_version("~> 1.50.5")},
       {:git_ops, "~> 2.4.5", only: :dev},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
       {:ex_check, "~> 0.11.0", only: :dev},
