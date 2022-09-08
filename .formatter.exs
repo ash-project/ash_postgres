1ccef5cc61e7d5dc5bb086bb787b83fa112c7d4e
Zach Daniel <zach@zachdaniel.dev>
2022-09-08 13:33:29 -0400

    chore: .formatter.exs typo

--- .formatter.exs
+++ .formatter.exs
@@ -44,8 +44,8 @@ spark_locals_without_parens = [
 
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: locals_without_parens,
+  locals_without_parens: spark_locals_without_parens,
   export: [
-    locals_without_parens: locals_without_parens
+    locals_without_parens: spark_locals_without_parens
   ]
 ]
