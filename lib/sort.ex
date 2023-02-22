d07083e64264d5c2854087b106f771e80412305c
Zach Daniel <zach@zachdaniel.dev>
2023-02-21 19:45:07 -0500

    improvement: add aggregates used by sorts

--- lib/sort.ex
+++ lib/sort.ex
@@ -12,121 +12,169 @@ defmodule AshPostgres.Sort do
       ) do
     query = AshPostgres.DataLayer.default_bindings(query, resource)
 
-    sort
-    |> sanitize_sort()
-    |> Enum.reduce_while({:ok, []}, fn
-      {order, %Ash.Query.Calculation{} = calc}, {:ok, query_expr} ->
-        type =
-          if calc.type do
-            AshPostgres.Types.parameterized_type(calc.type, [])
-          else
-            nil
+    used_aggregates =
+      Enum.flat_map(sort, fn
+        {%Ash.Query.Calculation{} = calculation, _} ->
+          case Ash.Filter.hydrate_refs(
+                 calculation.module.expression(calculation.opts, calculation.context),
+                 %{
+                   resource: resource,
+                   aggregates: %{},
+                   calculations: %{},
+                   public?: false
+                 }
+               ) do
+            {:ok, hydrated} ->
+              Ash.Filter.used_aggregates(hydrated)
+
+            _ ->
+              []
           end
 
-        calc.opts
-        |> calc.module.expression(calc.context)
-        |> Ash.Filter.hydrate_refs(%{
-          resource: resource,
-          aggregates: query.__ash_bindings__.aggregate_defs,
-          calculations: %{},
-          public?: false
-        })
-        |> Ash.Filter.move_to_relationship_path(relationship_path)
-        |> case do
-          {:ok, expr} ->
-            expr = AshPostgres.Expr.dynamic_expr(query, expr, query.__ash_bindings__, false, type)
+        {key, _} ->
+          case Ash.Resource.Info.aggregate(resource, key) do
+            nil ->
+              []
 
-            {:cont, {:ok, query_expr ++ [{order, expr}]}}
-
-          {:error, error} ->
-            {:halt, {:error, error}}
-        end
-
-      {order, sort}, {:ok, query_expr} ->
-        expr =
-          case find_aggregate_binding(query.__ash_bindings__.bindings, relationship_path, sort) do
-            {:ok, binding} ->
-              aggregate =
-                Ash.Resource.Info.aggregate(resource, sort) ||
-                  raise "No such aggregate for query aggregate #{inspect(sort)}"
-
-              {:ok, attribute_type} =
-                if aggregate.field do
-                  related = Ash.Resource.Info.related(resource, aggregate.relationship_path)
-
-                  attr = Ash.Resource.Info.attribute(related, aggregate.field)
-
-                  if attr && related do
-                    {:ok, AshPostgres.Types.parameterized_type(attr.type, attr.constraints)}
-                  else
-                    {:ok, nil}
-                  end
-                else
-                  {:ok, nil}
-                end
-
-              default_value =
-                aggregate.default || Ash.Query.Aggregate.default_value(aggregate.kind)
-
-              if is_nil(default_value) do
-                Ecto.Query.dynamic(field(as(^binding), ^sort))
-              else
-                if attribute_type do
-                  Ecto.Query.dynamic(
-                    coalesce(
-                      field(as(^binding), ^sort),
-                      type(^default_value, ^attribute_type)
-                    )
-                  )
-                else
-                  Ecto.Query.dynamic(coalesce(field(as(^binding), ^sort), ^default_value))
-                end
-              end
-
-            :error ->
-              aggregate = Ash.Resource.Info.aggregate(resource, sort)
-
-              {binding, sort} =
-                if aggregate && aggregate.kind == :first &&
-                     AshPostgres.Aggregate.single_path?(resource, aggregate.relationship_path) do
-                  {AshPostgres.Join.get_binding(resource, aggregate.relationship_path, query, [
-                     :left,
-                     :inner
-                   ]), aggregate.field}
-                else
-                  {binding, sort}
-                end
-
-              Ecto.Query.dynamic(field(as(^binding), ^sort))
+            aggregate ->
+              [aggregate]
           end
 
-        {:cont, {:ok, query_expr ++ [{order, expr}]}}
-    end)
-    |> case do
-      {:ok, []} ->
-        {:ok, query}
+        _ ->
+          []
+      end)
 
-      {:ok, sort_exprs} ->
-        if return_order_by? do
-          {:ok, order_to_fragments(sort_exprs)}
-        else
-          new_query = Ecto.Query.order_by(query, ^sort_exprs)
-
-          sort_expr = List.last(new_query.order_bys)
-
-          new_query =
-            new_query
-            |> Map.update!(:windows, fn windows ->
-              order_by_expr = %{sort_expr | expr: [order_by: sort_expr.expr]}
-              Keyword.put(windows, :order, order_by_expr)
-            end)
-            |> Map.update!(:__ash_bindings__, &Map.put(&1, :__order__?, true))
-
-          {:ok, new_query}
-        end
-
+    case AshPostgres.Aggregate.add_aggregates(query, used_aggregates, resource, false) do
       {:error, error} ->
         {:error, error}
+
+      {:ok, query} ->
+        sort
+        |> sanitize_sort()
+        |> Enum.reduce_while({:ok, []}, fn
+          {order, %Ash.Query.Calculation{} = calc}, {:ok, query_expr} ->
+            type =
+              if calc.type do
+                AshPostgres.Types.parameterized_type(calc.type, [])
+              else
+                nil
+              end
+
+            calc.opts
+            |> calc.module.expression(calc.context)
+            |> Ash.Filter.hydrate_refs(%{
+              resource: resource,
+              aggregates: query.__ash_bindings__.aggregate_defs,
+              calculations: %{},
+              public?: false
+            })
+            |> Ash.Filter.move_to_relationship_path(relationship_path)
+            |> case do
+              {:ok, expr} ->
+                expr =
+                  AshPostgres.Expr.dynamic_expr(query, expr, query.__ash_bindings__, false, type)
+
+                {:cont, {:ok, query_expr ++ [{order, expr}]}}
+
+              {:error, error} ->
+                {:halt, {:error, error}}
+            end
+
+          {order, sort}, {:ok, query_expr} ->
+            expr =
+              case find_aggregate_binding(
+                     query.__ash_bindings__.bindings,
+                     relationship_path,
+                     sort
+                   ) do
+                {:ok, binding} ->
+                  aggregate =
+                    Ash.Resource.Info.aggregate(resource, sort) ||
+                      raise "No such aggregate for query aggregate #{inspect(sort)}"
+
+                  {:ok, attribute_type} =
+                    if aggregate.field do
+                      related = Ash.Resource.Info.related(resource, aggregate.relationship_path)
+
+                      attr = Ash.Resource.Info.attribute(related, aggregate.field)
+
+                      if attr && related do
+                        {:ok, AshPostgres.Types.parameterized_type(attr.type, attr.constraints)}
+                      else
+                        {:ok, nil}
+                      end
+                    else
+                      {:ok, nil}
+                    end
+
+                  default_value =
+                    aggregate.default || Ash.Query.Aggregate.default_value(aggregate.kind)
+
+                  if is_nil(default_value) do
+                    Ecto.Query.dynamic(field(as(^binding), ^sort))
+                  else
+                    if attribute_type do
+                      Ecto.Query.dynamic(
+                        coalesce(
+                          field(as(^binding), ^sort),
+                          type(^default_value, ^attribute_type)
+                        )
+                      )
+                    else
+                      Ecto.Query.dynamic(coalesce(field(as(^binding), ^sort), ^default_value))
+                    end
+                  end
+
+                :error ->
+                  aggregate = Ash.Resource.Info.aggregate(resource, sort)
+
+                  {binding, sort} =
+                    if aggregate && aggregate.kind == :first &&
+                         AshPostgres.Aggregate.single_path?(resource, aggregate.relationship_path) do
+                      {AshPostgres.Join.get_binding(
+                         resource,
+                         aggregate.relationship_path,
+                         query,
+                         [
+                           :left,
+                           :inner
+                         ]
+                       ), aggregate.field}
+                    else
+                      {binding, sort}
+                    end
+
+                  Ecto.Query.dynamic(field(as(^binding), ^sort))
+              end
+
+            {:cont, {:ok, query_expr ++ [{order, expr}]}}
+        end)
+        |> case do
+          {:ok, []} ->
+            {:ok, query}
+
+          {:ok, sort_exprs} ->
+            if return_order_by? do
+              {:ok, order_to_fragments(sort_exprs)}
+            else
+              new_query = Ecto.Query.order_by(query, ^sort_exprs)
+
+              sort_expr = List.last(new_query.order_bys)
+
+              new_query =
+                new_query
+                |> Map.update!(:windows, fn windows ->
+                  order_by_expr = %{sort_expr | expr: [order_by: sort_expr.expr]}
+                  Keyword.put(windows, :order, order_by_expr)
+                end)
+                |> Map.update!(:__ash_bindings__, &Map.put(&1, :__order__?, true))
+
+              {:ok, new_query}
+            end
+
+          {:error, error} ->
+            {:error, error}
+        end
     end
   end
 
