75a5467d35a440f8b625abbe2b405ef119d6e0bd
Zach Daniel <zach@zachdaniel.dev>
2023-05-27 00:39:49 -0400

    test: format & update tests

--- test/bulk_create_test.exs
+++ test/bulk_create_test.exs
@@ -64,6 +64,111 @@ defmodule AshPostgres.BulkCreateTest do
                end)
     end
 
+    # confirmed that this doesn't work because it can't. An upsert must map to a potentially successful insert.
+    # leaving this test here for posterity
+    # test "bulk creates can upsert with id" do
+    #   org_id = Ash.UUID.generate()
+
+    #   _new_org =
+    #     Organization
+    #     |> Ash.Changeset.for_create(:create, %{
+    #       id: org_id,
+    #       title: "Avengers"
+    #     })
+    #     |> Api.create!()
+
+    #   assert [
+    #            {:ok,
+    #             %{
+    #               name: "Bruce Banner",
+    #               code: "BB01",
+    #               must_be_present: "I am Hulk",
+    #               organization_id: org_id
+    #             }},
+    #            {:ok,
+    #             %{
+    #               name: "Tony Stark",
+    #               code: "TS01",
+    #               must_be_present: "I am Iron Man",
+    #               organization_id: org_id
+    #             }}
+    #          ] =
+    #            Api.bulk_create!(
+    #              [
+    #                %{
+    #                  name: "Tony Stark",
+    #                  code: "TS01",
+    #                  must_be_present: "I am Iron Man",
+    #                  organization_id: org_id
+    #                },
+    #                %{
+    #                  name: "Bruce Banner",
+    #                  code: "BB01",
+    #                  must_be_present: "I am Hulk",
+    #                  organization_id: org_id
+    #                }
+    #              ],
+    #              Manager,
+    #              :create,
+    #              return_stream?: true,
+    #              return_records?: true,
+    #              return_errors?: true
+    #            )
+    #            |> Enum.sort_by(fn {:ok, result} -> result.name end)
+
+    #   Logger.configure(level: :debug)
+
+    #   assert [
+    #            {:ok,
+    #             %{
+    #               name: "Bruce Banner",
+    #               code: "BB01",
+    #               must_be_present: "I am Hulk",
+    #               organization_id: org_id,
+    #               role: "bone breaker"
+    #             }},
+    #            {:ok,
+    #             %{
+    #               name: "Tony Stark",
+    #               code: "TS01",
+    #               must_be_present: "I am Iron Man",
+    #               organization_id: org_id,
+    #               role: "master in chief"
+    #             }}
+    #          ] =
+    #            Api.bulk_create!(
+    #              [
+    #                %{
+    #                  name: "Tony Stark",
+    #                  code: "TS01",
+    #                  organization_id: org_id,
+    #                  role: "master in chief"
+    #                },
+    #                %{
+    #                  name: "Brice Brenner",
+    #                  code: "BB01",
+    #                  organization_id: org_id,
+    #                  role: "bone breaker"
+    #                }
+    #              ],
+    #              Manager,
+    #              :create,
+    #              upsert?: true,
+    #              upsert_identity: :uniq_code,
+    #              upsert_fields: [:role],
+    #              return_stream?: true,
+    #              return_records?: true,
+    #              return_errors?: true
+    #            )
+    #            |> Enum.sort_by(fn
+    #              {:ok, result} ->
+    #                result.name
+
+    #              _ ->
+    #                nil
+    #            end)
+    # end
+
     test "bulk creates can create relationships" do
       Api.bulk_create!(
         [%{title: "fred", rating: %{score: 5}}, %{title: "george", rating: %{score: 0}}],
