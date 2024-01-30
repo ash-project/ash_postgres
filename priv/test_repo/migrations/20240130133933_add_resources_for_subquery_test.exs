35db52879b5c0cdfeb00679738c73f79a5a115ea
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-01-30 14:54:45 +0100

    rename migration

--- priv/test_repo/migrations/20240130133933_add_resources_for_subquery_test.exs
+++ priv/test_repo/migrations/20240130133933_add_resources_for_subquery_test.exs
@@ -1,4 +1,4 @@
-defmodule AshPostgres.TestRepo.Migrations.MigrateResources15 do
+defmodule AshPostgres.TestRepo.Migrations.AddResourcesForSubqueryTest do
   @moduledoc """
   Updates resources based on their most recent snapshots.
 
