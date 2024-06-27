844d9b0717e2da64b45e5dd2706cf918739151b5
Zach Daniel <zach@zachdaniel.dev>
2024-06-27 13:48:56 -0400

    chore: fix 1.17 warning

--- lib/data_layer.ex
+++ lib/data_layer.ex
@@ -1549,7 +1549,7 @@ defmodule AshPostgres.DataLayer do
     ecto_changeset =
       case changeset.data do
         %Ash.Changeset.OriginalDataNotAvailable{} ->
-          changeset.resource.__struct__
+          changeset.resource.__struct__()
 
         data ->
           data
