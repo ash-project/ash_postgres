ca27613f9f0efcb069bb3151cbf14fa9559e26b7
Zach Daniel <zach@zachdaniel.dev>
2024-07-09 20:42:04 -0400

    chore: remove unused function

--- lib/mix/tasks/ash_postgres.install.ex
+++ lib/mix/tasks/ash_postgres.install.ex
@@ -247,44 +247,6 @@ defmodule Mix.Tasks.AshPostgres.Install do
     )
   end
 
-  defp add_test_supports_to_elixirc_paths(igniter) do
-    Igniter.update_elixir_file(igniter, "mix.exs", fn zipper ->
-      with {:ok, zipper} <- Igniter.Code.Module.move_to_module_using(zipper, Mix.Project),
-           {:ok, zipper} <- Igniter.Code.Module.move_to_def(zipper, :project, 0),
-           {:ok, zipper} <-
-             Igniter.Code.Common.move_right(zipper, &Igniter.Code.List.list?/1) do
-        case Igniter.Code.List.move_to_list_item(
-               zipper,
-               &Kernel.match?({:elixirc_paths, _}, &1)
-             ) do
-          {:ok, zipper} ->
-            Sourceror.Zipper.top(zipper)
-
-          _ ->
-            with {:ok, zipper} <-
-                   Igniter.Code.List.append_to_list(
-                     zipper,
-                     quote(do: {:elixirc_paths, elixirc_paths(Mix.env())})
-                   ),
-                 zipper <- Sourceror.Zipper.top(zipper),
-                 {:ok, zipper} <- Igniter.Code.Common.move_to_do_block(zipper),
-                 zipper <-
-                   Igniter.Code.Common.add_code(
-                     zipper,
-                     "defp elixirc_paths(:test), do: [\"lib\", \"test/support\"]"
-                   ),
-                 zipper <-
-                   Igniter.Code.Common.add_code(
-                     zipper,
-                     "defp elixirc_paths(_), do: [\"lib\"]"
-                   ) do
-              zipper
-            end
-        end
-      end
-    end)
-  end
-
   defp setup_repo_module(igniter, otp_app, repo) do
     default_repo_contents =
       """
