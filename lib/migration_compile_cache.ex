a9f1a00316def8e2f30ca652a5a074c29ddf7d91
skrioify <emil@nylind.se>
2024-10-18 13:04:00 +0200

    fix: race condition compiling migrations when concurrently creating new tenants (#406)

---
+++ lib/migration_compile_cache.ex
@@ -0,0 +1,38 @@
+defmodule AshPostgres.MigrationCompileCache do
+  @moduledoc """
+  A cache for the compiled migrations.
+
+  This is used to avoid recompiling the migration files
+  every time a migration is run, as well as ensuring that
+  migrations are compiled sequentially.
+
+  This is important because otherwise there is a race condition
+  where two invocations could be compiling the same migration at
+  once, which would error out.
+  """
+
+  def start_link(opts \\ %{}) do
+    Agent.start_link(fn -> opts end, name: __MODULE__)
+  end
+
+  @doc """
+  Compile a file, caching the result for future calls.
+  """
+  def compile_file(file) do
+    Agent.get_and_update(__MODULE__, fn state -> 
+      new_state = ensure_compiled(state, file)
+      {Map.get(new_state, file), new_state}
+    end)
+  end
+
+  defp ensure_compiled(state, file) do
+    case Map.get(state, file) do
+      nil ->
+        compiled = Code.compile_file(file)
+        Map.put(state, file, compiled)
+      _ ->
+        state
+    end
+  end
+
+end
