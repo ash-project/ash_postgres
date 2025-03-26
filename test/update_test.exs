11278a7fbc4e582f784244c365ab78d450a14d73
Jechol Lee <mr.jechol@gmail.com>
2025-03-26 10:29:46 +0900

    Add test: loaded relationships remain loaded after update

--- test/update_test.exs
+++ test/update_test.exs
@@ -218,4 +218,25 @@ defmodule AshPostgres.UpdateTest do
 
     assert is_nil(post.author)
   end
+
+  test "loaded relationships remain loaded after update" do
+    author =
+      AshPostgres.Test.Author
+      |> Ash.Changeset.for_create(:create, %{first_name: "foo", last_name: "bar"})
+      |> Ash.create!()
+
+    _post =
+      AshPostgres.Test.Post
+      |> Ash.Changeset.for_create(:create, %{title: "baz"})
+      |> Ash.Changeset.manage_relationship(:author, author, type: :append_and_remove)
+      |> Ash.create!()
+
+    author = author |> Ash.load!(:posts)
+
+    assert [%Post{}] = author.posts
+
+    author = author |> Ash.Changeset.for_update(:update, %{first_name: "foo2"}) |> Ash.update!()
+
+    assert [%Post{}] = author.posts
+  end
 end
