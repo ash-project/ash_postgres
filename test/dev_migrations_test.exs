7d6f362b09c7c3e98f0e34273afd56856054a54d
ken-kost <kenneth.kostresevic@gmail.com>
2025-03-30 16:22:01 +0200

    WIP

--- test/dev_migrations_test.exs
+++ test/dev_migrations_test.exs
@@ -125,12 +125,88 @@ defmodule AshPostgres.DevMigrationsTest do
     end
   end
 
+  describe "--dev option tenant" do
+    setup do
+      on_exit(fn ->
+        resource_dev_path = "priv/resource_snapshots/dev_test_repo"
+        resource_files = File.ls!(resource_dev_path)
+        Enum.each(resource_files, &File.rm_rf!(Path.join(resource_dev_path, &1)))
+        migrations_dev_path = "priv/dev_test_repo/migrations"
+        migration_files = File.ls!(migrations_dev_path)
+        Enum.each(migration_files, &File.rm!(Path.join(migrations_dev_path, &1)))
+        tenant_migrations_dev_path = "priv/dev_test_repo/tenant_migrations"
+        tenant_migration_files = File.ls!(tenant_migrations_dev_path)
+        Enum.each(tenant_migration_files, &File.rm!(Path.join(tenant_migrations_dev_path, &1)))
+      end)
+    end
+
+    test "rolls back dev migrations before deleting" do
+      defposts do
+        postgres do
+          schema("example")
+        end
+
+        attributes do
+          uuid_primary_key(:id)
+          attribute(:title, :string, public?: true, primary_key?: true, allow_nil?: false)
+        end
+
+        multitenancy do
+          strategy(:context)
+        end
+      end
+
+      defdomain([Post])
+      capture_log(fn -> tenant_migrate() end) |> dbg()
+
+      AshPostgres.MigrationGenerator.generate(Domain,
+        snapshot_path: "priv/resource_snapshots",
+        migration_path: "priv/dev_test_repo/migrations",
+        tenant_migration_path: "priv/dev_test_repo/tenant_migrations",
+        dev: true
+      )
+
+      assert [] =
+               Enum.sort(
+                 Path.wildcard("priv/dev_test_repo/migrations/**/*_migrate_resources*.exs")
+               )
+               |> Enum.reject(&String.contains?(&1, "extensions"))
+
+      assert [_tenant_migration] =
+               Enum.sort(
+                 Path.wildcard("priv/dev_test_repo/tenant_migrations/**/*_migrate_resources*.exs")
+               )
+               |> Enum.reject(&String.contains?(&1, "extensions"))
+
+      assert capture_log(fn -> tenant_migrate() end) =~ "create table posts"
+      assert capture_log(fn -> tenant_migrate() end) =~ "create table posts"
+
+      AshPostgres.MigrationGenerator.generate(Domain,
+        snapshot_path: "priv/resource_snapshots",
+        migration_path: "priv/dev_test_repo/migrations",
+        tenant_migration_path: "priv/dev_test_repo/tenant_migrations"
+      )
+
+      assert capture_log(fn -> tenant_migrate() end) =~ "create table posts"
+    end
+  end
+
   defp migrate do
     Mix.Tasks.AshPostgres.Migrate.run([
       "--migrations-path",
       "priv/dev_test_repo/migrations",
       "--repo",
       "AshPostgres.DevTestRepo"
+    ])
+  end
+
+  defp tenant_migrate do
+    Mix.Tasks.AshPostgres.Migrate.run([
+      "--migrations-path",
+      "priv/dev_test_repo/tenant_migrations",
+      "--repo",
+      "AshPostgres.DevTestRepo",
+      "--tenants"
     ])
   end
 end
