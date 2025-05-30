be48da96af1d08614d725053b9ea8c088aee79b8
Frank Dugan III <frank3@duganator.com>
2025-05-30 17:03:44 -0500

    fix: retain repo as atom in migrator task (#560)

--- lib/mix/tasks/ash_postgres.migrate.ex
+++ lib/mix/tasks/ash_postgres.migrate.ex
@@ -130,16 +130,9 @@ defmodule Mix.Tasks.AshPostgres.Migrate do
           for tenant <- tenants(repo, opts) do
             rest_opts = AshPostgres.Mix.Helpers.delete_arg(rest_opts, "--prefix")
 
-            repo =
-              if is_atom(repo) do
-                inspect(repo)
-              else
-                repo
-              end
-
             Mix.Task.run(
               "ecto.migrate",
-              ["-r", repo] ++
+              ["-r", to_string(repo)] ++
                 rest_opts ++
                 ["--prefix", tenant, "--migrations-path", tenant_migrations_path(opts, repo)]
             )
