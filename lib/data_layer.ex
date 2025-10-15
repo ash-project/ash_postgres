f44cee5e185da7dff4ddb26b8bd65bbc78e75238
Zach Daniel <zach@zachdaniel.dev>
2025-10-14 22:23:15 -0400

    chore: check `upsert?` as well inside of new code
    not technically necessary but doesn't hurt

--- lib/data_layer.ex
+++ lib/data_layer.ex
@@ -2042,7 +2042,7 @@ defmodule AshPostgres.DataLayer do
       # if it's single the return_skipped_upsert? is handled at the
       # call site https://github.com/ash-project/ash_postgres/blob/0b21d4a99cc3f6d8676947e291ac9b9d57ad6e2e/lib/data_layer.ex#L3046-L3046
       result =
-        if options[:return_skipped_upsert?] && !opts[:single?] do
+        if options[:upsert?] && options[:return_skipped_upsert?] && !opts[:single?] do
           [changeset | _] = changesets
 
           results_by_identity =
