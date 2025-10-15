f48e1a4f0a210c822e564c8834e5a494272b5562
Zach Daniel <zach@zachdaniel.dev>
2025-10-15 18:51:47 -0400

    chore: update ash_sql

--- mix.exs
+++ mix.exs
@@ -179,7 +179,7 @@ defmodule AshPostgres.MixProject do
     [
       {:ash, ash_version("~> 3.7")},
       {:spark, "~> 2.3 and >= 2.3.4"},
-      {:ash_sql, ash_sql_version("~> 0.3 and >= 0.3.6")},
+      {:ash_sql, ash_sql_version("~> 0.3 and >= 0.3.7")},
       {:igniter, "~> 0.6 and >= 0.6.29", optional: true},
       {:ecto_sql, "~> 3.13"},
       {:ecto, "~> 3.13"},
