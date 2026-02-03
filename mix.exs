769aa513b094f9d1980d1e93cd3af545ebbafe5c
Zach Daniel <zach@zachdaniel.dev>
2026-02-03 16:44:05 -0500

    chore: update mix.exs

--- mix.exs
+++ mix.exs
@@ -185,9 +185,9 @@ defmodule AshPostgres.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ash, ash_version("~> 3.11 and >= 3.11.2")},
+      {:ash, ash_version("~> 3.14")},
       {:spark, "~> 2.3 and >= 2.3.4"},
-      {:ash_sql, ash_sql_version("~> 0.3 and >= 0.3.16")},
+      {:ash_sql, ash_sql_version("~> 0.4 and >= 0.4.2")},
       {:igniter, "~> 0.6 and >= 0.6.29", optional: true},
       {:ecto_sql, "~> 3.13"},
       {:ecto, "~> 3.13"},
