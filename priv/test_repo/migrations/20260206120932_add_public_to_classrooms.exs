525d47bdc6851a6c203deb1d62c3cac5c6f508c7
Jechol Lee <mr.jechol@gmail.com>
2026-04-16 16:39:51 +0900

    Add missing REUSE headers for test snapshots

--- priv/test_repo/migrations/20260206120932_add_public_to_classrooms.exs
+++ priv/test_repo/migrations/20260206120932_add_public_to_classrooms.exs
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2019 ash_postgres contributors <https://github.com/ash-project/ash_postgres/graphs/contributors>
+#
+# SPDX-License-Identifier: MIT
+
 defmodule AshPostgres.TestRepo.Migrations.AddPublicToClassrooms do
   @moduledoc """
   Adds public column to classrooms for through relationship policy testing.
